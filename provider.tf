6a345f8f06b7e221e02659fd8680e4acc24d0a0f
Tim Jacomb <tim.jacomb@hmcts.net>
2022-08-23 12:25:22 +0100

    Fmt

--- provider.tf
+++ provider.tf
@@ -12,11 +12,11 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.19.1"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.3.2"
     }
   }
