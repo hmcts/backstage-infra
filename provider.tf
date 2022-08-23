2597f036618015044450a2334eb1f0ab7ae45c3a
Tim Jacomb <tim.jacomb@hmcts.net>
2022-08-23 12:20:31 +0100

    Resolve deprecation warning

--- provider.tf
+++ provider.tf
@@ -9,4 +9,15 @@ provider "random" {
 
 terraform {
   backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.19.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
 }
