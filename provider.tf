313a67895375e696bedfe514458f1ec873c0e509
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-08-26 12:36:20 +0100

    DTSPO-33736 - pin azurerm version to v4.x

--- provider.tf
+++ provider.tf
@@ -17,10 +17,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 4.0"
+      version = "3.3.2"
     }
   }
 }
